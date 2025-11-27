1580e2b3f415b10261c59d21fb483bada9c0397d
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-11-27 23:42:42 +0900

    Article CRUD API 스켈레톤 추가

---
+++ service/article/src/test/java/kuke/board/article/ArticleApiTest.java
@@ -0,0 +1,15 @@
+package kuke.board.article;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class ArticleApiTest {
+    RestClient client = RestClient.create("http://localhost:9000");
+
+    @Test
+    void createTest() {
+
+    }
+
+
+}
