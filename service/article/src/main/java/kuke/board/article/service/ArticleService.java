88cd8ec1122555fc578ce04eb04f8b697e35ee2a
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-12-04 23:36:35 +0900

    게시글 수 구현

--- service/article/src/main/java/kuke/board/article/service/ArticleService.java
+++ service/article/src/main/java/kuke/board/article/service/ArticleService.java
@@ -1,7 +1,9 @@
 package kuke.board.article.service;
 
 import kuke.board.article.entity.Article;
+import kuke.board.article.entity.BoardArticleCount;
 import kuke.board.article.repository.ArticleRepository;
+import kuke.board.article.repository.BoardArticleCountRepository;
 import kuke.board.article.service.request.ArticleCreateRequest;
 import kuke.board.article.service.request.ArticleUpdateRequest;
 import kuke.board.article.service.response.ArticlePageResponse;
@@ -19,11 +21,18 @@ import java.util.stream.Collectors;
 public class ArticleService {
     private final Snowflake snowflake = new Snowflake();
     private final ArticleRepository articleRepository;
+    private final BoardArticleCountRepository boardArticleCountRepository;
 
     @Transactional
     public ArticleResponse save(ArticleCreateRequest request) {
         Article saved = articleRepository.save(Article.create(snowflake.nextId(), request.getTitle(), request.getContent(), request.getBoardId(), request.getWriterId()));
 
+        int result = boardArticleCountRepository.increase(request.getBoardId());
+        if (result == 0) {
+            boardArticleCountRepository.save(
+                    BoardArticleCount.init(request.getBoardId(), 1L)
+            );
+        }
         return ArticleResponse.from(saved);
     }
 
