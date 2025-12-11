7608a2bba19e11eafa1e52b12334c1b5256afa96
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-12-11 23:03:17 +0900

    Redis 분산락을 이용한 조회수 어뷰징 방지 구현

--- service/view/src/main/java/kuke/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/kuke/board/view/service/ArticleViewService.java
@@ -1,17 +1,26 @@
 package kuke.board.view.service;
 
 import kuke.board.view.repository.ArticleViewCountRepository;
+import kuke.board.view.repository.ArticleViewDistributeLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
     private final ArticleViewCountRepository articleViewCountRepository;
     private final ArticleViewCountBackupProcessor articleViewCountBackupProcessor;
+    private final ArticleViewDistributeLockRepository articleViewDistributeLockRepository;
+
     private static final int BACKUP_BATCH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
 
     public Long increase(Long articleId, Long userId) {
+        if (!articleViewDistributeLockRepository.lock(articleId, userId, TTL)) {
+            return count(articleId);
+        }
         Long count = articleViewCountRepository.increase(articleId);
         if (count % BACKUP_BATCH_SIZE == 0) {
             articleViewCountBackupProcessor.backup(articleId, count);
