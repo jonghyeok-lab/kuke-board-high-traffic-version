061cc8bb7436dc9699907abcde839db8f7b40751
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-12-16 22:09:20 +0900

    인기글 Consumer 구현 - Redis 에 상위 N건만 유지

---
+++ service/hot-article/src/main/java/kuke/board/hotarticle/config/KafkaConfig.java
@@ -0,0 +1,20 @@
+package kuke.board.hotarticle.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
+            ConsumerFactory<String, String> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL); // yml에 auto-commit false 시, 리스너가 이벤트 처리 후, ack 를 어떻게 호출할지
+        return factory;
+    }
+}
